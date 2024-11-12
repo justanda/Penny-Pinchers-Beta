be0b3cc5761112cc54839f4a8c5f0ac4407bb28d
Seth D Joslin <seth.joslin@yahoo.com>
2024-11-12 16:48:20 -0600

    edited seeds

--- server/src/seeds/orders-seeds.ts
+++ server/src/seeds/orders-seeds.ts
@@ -1,21 +1,21 @@
-import { order } from "../models/index.js";
+// import { order } from "../models/index.js";
 
-export const seedOrders = async () => {
-  await order.bulkCreate([
-    {
-      customerId: 1,
-      itemsId: 1,
-      shipped: false,
-    },
-    {
-      customerId: 2,
-      itemsId: 2,
-      shipped: false,
-    },
-    {
-      customerId: 3,
-      itemsId: 3,
-      shipped: false,
-    },
-  ]);
-};
+// export const seedOrders = async () => {
+//   await order.bulkCreate([
+//     {
+//       customerId: 1,
+//       itemsId: 1,
+//       shipped: false,
+//     },
+//     {
+//       customerId: 2,
+//       itemsId: 2,
+//       shipped: false,
+//     },
+//     {
+//       customerId: 3,
+//       itemsId: 3,
+//       shipped: false,
+//     },
+//   ]);
+// };
